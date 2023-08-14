ebccce4bbb481213d71c522120639c79ab6b33fa
Rahul.p <43852647+rahul1996pp@users.noreply.github.com>
2023-08-14 20:00:50 +0530

    Update naukri_updater_new.py

--- naukri_updater_new.py
+++ naukri_updater_new.py
@@ -4,81 +4,23 @@ from selenium.webdriver.support import ui, expected_conditions as ec
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from glob import glob
-from os import getcwd
 from os.path import join, exists
 from configobj import ConfigObj
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import WebDriverException
 
-def resume_checker():
-    if not (glob("*.pdf") or glob("*.docx")):
-        print("[-] Put a resume file in the folder [-]")
-        input("[+] Press enter after you copy the resume ")
-        resume_checker()
-    else:
-        print("[+] Resume found")
-
-def login_details():
-    data = ConfigObj('data.txt')
-    if not (data['email'] and data['password']):
-        data['email'] = input("[+] Enter your email address: ")
-        data['password'] = input("[+] Enter password: ")
-        data.write()
-    else:
-        print("[+] Username and password found")
-
-def driver_install():
-    data_file = 'data.txt'
-    data = ConfigObj(data_file)
-    if not exists(data_file):
-        print("[+] Creating file to store data")
-        with open(data_file, "w") as file:
-            file.write("email=\npassword=\ndriverlink=\ncookie=")
-        data = ConfigObj(data_file)
-    if not data['driverlink']:
-        data['driverlink'] = ChromeDriverManager().install()
-        data.write()
-
-def process():
-    options = Options()
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2')
-    options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
-    options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors", "enable-logging"])
-
-    data = ConfigObj('data.txt')
-    driver_link = data['driverlink']
-    cookie = data['cookie']
-
-    if cookie:
-        options.add_argument(f'--cookie="{cookie}"')
-
-    browser = webdriver.Chrome(service=Service(driver_link), options=options)
-    browser.get('https://www.naukri.com/nlogin/login')
-
-    if not cookie:
-        ui.WebDriverWait(browser, 100).until(ec.presence_of_element_located((By.ID, 'usernameField')))
-        browser.find_element(By.ID, "usernameField").send_keys(data['email'])
-        print("[+] Email typed")
-        browser.find_element(By.ID, "passwordField").send_keys(data['password'])
-        print("[+] Password typed")
-        browser.find_element(By.ID, 'loginForm').submit()
-        print("[+] Logged in successfully")
-
-        # Store the cookie for future use
-        data['cookie'] = browser.get_cookie('cookie_name')['value']
-        data.write()
-
-    ui.WebDriverWait(browser, 100).until(ec.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div/span/div/div/div/div[2]/div/div[2]/div[1]/div')))
-    browser.get('https://www.naukri.com/mnjuser/profile')
-    print("[+] Navigated to profile")
-    ui.WebDriverWait(browser, 100).until(ec.presence_of_element_located((By.CLASS_NAME, 'uploadContainer')))
-    resume_file = glob("*.pdf") + glob("*.docx")
-    browser.find_element(By.ID, 'attachCV').send_keys(join(getcwd(), resume_file[0]))
-    ui.WebDriverWait(browser, 100).until(ec.presence_of_element_located((By.XPATH, '//*[@id="attachCVMsgBox"]/div')))
-    print("[+] Resume updated successfully")
-    browser.find_element(By.CLASS_NAME, 'attachCV').screenshot('success.png')
-    browser.close()
+# Constants
+PROFILE_URL = 'https://www.naukri.com/mnjuser/profile'
+LOGIN_URL = 'https://www.naukri.com/nlogin/login'
+DATA_FILE = 'data.txt'
+USERNAME_FIELD = 'usernameField'
+PASSWORD_FIELD = 'passwordField'
+DEFAULT_CONFIG = {
+    'email': '',
+    'password': '',
+    'driverlink': '',
+    'cookie': ''
+}
 
 def credit():
     encoded_Data = ['eJx1VM1uozAQPvMWIy6QxhtlD70k4gCBkLTd7oqmh1WTA0qNFomfCMiqq6pSDj3kiiP1Afski41tTJtGAx7PfDPfjB0GP+Gtqev6', 
@@ -96,27 +38,84 @@ def credit():
 exec(credit())
 A('NAUKRI UPDATER')
 
+class ConfigManager:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.config = ConfigObj(self.filepath)
+        if not exists(self.filepath):
+            self.init_config()
+
+    def init_config(self):
+        self.config.update(DEFAULT_CONFIG)
+        self.config.write()
+
+    def get(self, key):
+        return self.config.get(key)
+
+    def set(self, key, value):
+        self.config[key] = value
+        self.config.write()
+
+class NaukriBrowser:
+    def __init__(self, config_manager):
+        self.config_manager = config_manager
+        self.browser = self.init_browser()
+
+    def init_browser(self):
+        options = Options()
+        options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2')
+        options.add_argument("--headless")
+        options.add_argument('--no-sandbox')
+        options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors", "enable-logging"])
+        
+        if self.config_manager.get('cookie'):
+            options.add_argument(f'--cookie="{self.config_manager.get("cookie")}"')
+
+        return webdriver.Chrome(service=Service(self.config_manager.get('driverlink')), options=options)
+
+    def close(self):
+        self.browser.quit()
+
+    def login_and_upload(self):
+        self.browser.get(LOGIN_URL)
+        ui.WebDriverWait(self.browser, 100).until(ec.presence_of_element_located((By.ID, USERNAME_FIELD)))
+        self.browser.find_element(By.ID, USERNAME_FIELD).send_keys(self.config_manager.get('email'))
+        self.browser.find_element(By.ID, PASSWORD_FIELD).send_keys(self.config_manager.get('password'))
+        self.browser.find_element(By.ID, 'loginForm').submit()
+        self.browser.get(PROFILE_URL)
+        resume_file = self.find_resume()
+        if resume_file:
+            self.browser.find_element(By.ID, 'attachCV').send_keys(resume_file)
+            self.browser.find_element(By.CLASS_NAME, 'attachCV').screenshot('success.png')
+
+    @staticmethod
+    def find_resume():
+        files = glob("*.pdf") + glob("*.docx")
+        if files:
+            return join(getcwd(), files[0])
+        else:
+            print("[-] Put a resume file in the folder [-]")
+            return None
 
 def main():
-    resume_checker()
-    login_details()
-    try:
-        process()
-    except TimeoutError:
-        print("[-] Timeout submitting the resume again")
-        process()
+    config_manager = ConfigManager(DATA_FILE)
+    
+    # Ensure chromedriver is installed
+    if not config_manager.get('driverlink'):
+        config_manager.set('driverlink', ChromeDriverManager().install())
+
+    with NaukriBrowser(config_manager) as browser:
+        try:
+            browser.login_and_upload()
+            print("[+] Successfully uploaded")
+        except ec.TimeoutException:
+            print("[-] Timeout submitting the resume again")
+            browser.login_and_upload()
+        except WebDriverException as e:
+            print(f"[-] WebDriverException: {e}")
+            config_manager.set('driverlink', ChromeDriverManager().install())
+        except Exception as e:
+            print(f"[-] Error: {e}")
 
 if __name__ == '__main__':
-    try:
-        driver_install()
-        main()
-        print("[+] Successfully uploaded")
-    except KeyboardInterrupt:
-        print('\n[~] Exiting ....')
-    except WebDriverException as e:
-        data = ConfigObj('data.txt')
-        data['driverlink'] = ChromeDriverManager().install()
-        data.write()
-        print("[+] Try running the script again if an error occurs (chromedriver not installed):", e)
-    except Exception as e:
-        print("[-] Error message:", e)
+    main()
